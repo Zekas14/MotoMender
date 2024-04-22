dc6623c1676ab99b70ff489d08b2c4d0823073f5
loai-cpu3 <loai20189@gmail.com>
2024-04-22 20:20:01 +0200

    added Product CURD

---
+++ Routes/productRoutes.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const productController = require("../Apis/productServices");
+
+const productRouter = express.Router();
+
+productRouter
+  .route("/")
+  .get(productController.getProduct)
+  .post(productController.createProduct);
+productRouter
+  .route("/:id")
+  .patch(productController.updateProduct)
+  .delete(productController.deleteProduct);
+
+module.exports = productRouter;
--- app.js
+++ app.js
@@ -1,7 +1,8 @@
-const userRoutes = require('./Routes/userRoutes');
-const express = require('express');
+const userRoutes = require("./Routes/userRoutes");
+const productRouter = require("./Routes/productRoutes");
+const express = require("express");
 const app = express();
 app.use(express.json());
-app.use('/user',userRoutes);
-
+app.use("/user", userRoutes);
+app.use("/products", productRouter);
 module.exports = app;
