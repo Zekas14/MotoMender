08c71d055e74146a2b53152ff1fdc7e9fa79cfb1
Ahmed Zakarya <ahmedzrks123@gmail.com>
2024-05-04 17:26:21 +0200

    Add Email Verification

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const app = express();
 app.use(express.json());
 app.use('/imgs/userImages', express.static(path.join(__dirname, 'public/imgs/userImages')));
 app.use("/imgs/products", express.static("public/imgs/products"));
-app.use("/user", authRoutes);
+app.use("/auth", authRoutes);
 app.use("/admin", adminRoutes);
 app.use("/products", productRouter);
 app.use("/review", ReviewRoutes);
