b7c4bd0b0ecc967206f2ba7e8227ec4c4cc158e1
Ahmed Zakarya <ahmedzrks123@gmail.com>
2024-05-05 17:29:27 +0200

    create Resend Verification Code Api for Log In Proccess

--- Routes/authRoutes.js
+++ Routes/authRoutes.js
@@ -7,14 +7,16 @@ router.post("/register", authServices.register);
 router.post("/forgetPassword", authServices.forgetPassword);
 router.patch("/resetPassword", authServices.resetPassword);
 router.post("/login", authServices.logIn);
+router.post("/resendEmailVerification", authServices.resendEmailVerification);
+
 router.post("/google",authServices.signUpWithGoogle);
 
-// router.use(authServices.protect);
-// router.use(authServices.retrictTo("admin"));
+router.use(authServices.protect);
+router.use(authServices.retrictTo("admin"));
 
 router.delete("/deleteAccount/:userId", authServices.deleteAccount);
 router.put("/updateAccount/:userId", authServices.updateAccount);
-// router.use(authServices.retrictTo("admin", "user"));
+router.use(authServices.retrictTo("admin", "user"));
 
 router.post("/uploadProfileImage/:userId", authServices.uploadProfileImage);
 router.patch("/updatePassword", authServices.updatePassword);
