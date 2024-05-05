44be8ca0cd952914c9b7465547088e5c655fff66
MoBata <mohamedelkarn2003@gmail.com>
2024-05-05 19:13:26 +0300

    testing verfiyEmail with seprate Method

--- Routes/authRoutes.js
+++ Routes/authRoutes.js
@@ -8,14 +8,16 @@ router.patch('/verfiyEmail',authServices.emailVerfied);
 router.post("/forgetPassword", authServices.forgetPassword);
 router.patch("/resetPassword", authServices.resetPassword);
 router.post("/login", authServices.logIn);
+router.post("/resendEmailVerification", authServices.resendEmailVerification);
+
 router.post("/google",authServices.signUpWithGoogle);
 
-// router.use(authServices.protect);
-// router.use(authServices.retrictTo("admin"));
+router.use(authServices.protect);
+router.use(authServices.retrictTo("admin"));
 
 router.delete("/deleteAccount/:userId", authServices.deleteAccount);
-router.put("/updateAccount/:userId", authServices.updateAccount);
-// router.use(authServices.retrictTo("admin", "user"));
+router.put("/updateAccount", authServices.updateAccount);
+router.use(authServices.retrictTo("admin", "user"));
 
 router.post("/uploadProfileImage/:userId", authServices.uploadProfileImage);
 router.patch("/updatePassword", authServices.updatePassword);
